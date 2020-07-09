148c4e38135535f0090602c176f6ab9e8f2e549e
trampboy495 <histroylover@gmail.com>
2020-07-09 12:15:06 +0530

    added UC Employee Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 
 echo "Welcome to Employmee wage calculation program"
+isPresent=1
+isAbsent=0
+empCheck=$(( RANDOM % 2 ))
+if [ $empCheck -eq $isPresent ]
+then
+	echo "employee is present"
+else
+	echo "employee is not present"
+fi
